4f821410c95f6c3ab69c1e1de876b9ec86567a15
Brian Sanchez <briansanchezleonel@gmail.com>
2025-03-14 13:19:05 -0300

    Version 1.1

--- Js/script.js
+++ Js/script.js
@@ -2,7 +2,6 @@ var swiper = new Swiper(".slide-content", {
     slidesPerView: 1,
     centeredSlides: false,
     slidesPerGroupSkip: 1,
-    grabCursor: true,
     keyboard: {
         enabled: true,
     },
--- Js/script2.js
+++ Js/script2.js
@@ -1,6 +1,5 @@
 var swiper = new Swiper(".mySwiper", {
     effect: "flip",
-    grabCursor: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
